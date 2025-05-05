6adfb001e772c71d67a7a9b32e16d03b9245f704
demian-gg <hello@demian.gg>
2025-05-05 17:22:09 +0200

    Remove redundant props for Box component

--- src/components/ui/Box.tsx
+++ src/components/ui/Box.tsx
@@ -1,16 +1,11 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-interface BoxProps {
-  style?: React.CSSProperties;
-  className?: string;
-}
-
-const Box = styled.div<BoxProps>`
-  background-color: #98845C;
-  border-top: 2px solid #D4BC94;
-  border-left: 2px solid #D4BC94;
-  border-bottom: 2px solid #68502C;
-  border-right: 2px solid #68502C;
+const Box = styled.div`
+  background-color: #98845c;
+  border-top: 2px solid #d4bc94;
+  border-left: 2px solid #d4bc94;
+  border-bottom: 2px solid #68502c;
+  border-right: 2px solid #68502c;
   box-shadow: 3px 3px 0px rgba(0, 0, 0, 0.4);
   padding: 10px;
 `;
